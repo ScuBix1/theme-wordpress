9f043ad360a30920850e4a7d30b75b4573e74e68
ScuBix1 <bastian.monnin1@gmail.com>
2024-07-22 22:36:59 +0200

    add bootstrap

--- functions.php
+++ functions.php
@@ -8,4 +8,11 @@ if(!function_exists("post_thumbnails_add")){
     }
     add_action("after_setup_theme", "post_thumbnails_add");
 }
+if(!function_exists("theme_entrainement_enqueue")){
+    function theme_entrainement_enqueue(){
+        wp_enqueue_style('bootstrapCss', get_stylesheet_uri('assets/css/bootstrap.min.css', __FILE__));
+        wp_enqueue_script('bootstrapJs', get_template_directory_uri().'/assets/js/bootstrap.min.js', array('jquery'));
+    }
+    add_action('wp_enqueue_scripts', 'theme_entrainement_enqueue');
+}
 ?>
